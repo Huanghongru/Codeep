1c820c66c36ef34533518d13945acbf07713c05a
huanghongru <huanghongru1997@gmail.com>
2018-06-19 08:20:47 -0700

    implement basic frame of calling

--- src/core.cpp
+++ src/core.cpp
@@ -1,26 +1,42 @@
 #include "core.hpp"
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <signal.h>
+#include <sys/types.h>
 
 namespace Codeep {
 namespace Core {
 
-pid_t daemon_pid;
-int a;
-
 void Init() {
-    if (daemon(1, 1) == -1)
+    if (daemon(1, 1) == -1) {
+        std::cout << "Something wrong ... :(" << std::endl;
         exit(-1);
-    std::cout << "hello again" << std::endl;
-    daemon_pid = getpid();
-    std::cout << daemon_pid << std::endl;
+    }
+    // daemon_pid = fork();
+    pid_t daemon_pid = getpid();
+    std::ofstream tmpfile;
+    tmpfile.open("tmppid.txt");
+    tmpfile << daemon_pid;
+    tmpfile.close();
 
-    a = 10;
+    std::cout << "Codeep has been successfully launched !!! \n"
+              << "Enjoy coding !!!" << std::endl;
 
     while (true) {}
 }
 
 void Exit() {
-    std::cout << a << std::endl;
+    std::ifstream tmpfile;
+    tmpfile.open("tmppid.txt");
+    pid_t pid;
+    tmpfile >> pid;
+    tmpfile.close();
+    remove("tmppid.txt");
+
+    kill(pid, 9);
+
+    std::cout << "Coding finished !!!" << std::endl;
 }
 
 }
