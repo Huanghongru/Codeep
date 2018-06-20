adeac7b7309689065ce307ee9f6764d01cc5a9be
huanghongru <huanghongru1997@gmail.com>
2018-06-20 20:31:57 +0800

    add keystrokes capture function

--- src/core.cpp
+++ src/core.cpp
@@ -1,19 +1,23 @@
 #include "core.hpp"
+#include "utils.hpp"
+
 #include <iostream>
-#include <stdio.h>
 #include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <assert.h>
 
 namespace Codeep {
 namespace Core {
 
+typedef struct input_event input_event;
+char* LOGFILE = "./codeep.log";
+
 void Init() {
-    if (daemon(1, 1) == -1) {
-        std::cout << "Something wrong ... :(" << std::endl;
-        exit(-1);
-    }
-    // daemon_pid = fork();
+   // daemon_pid = fork();
     pid_t daemon_pid = getpid();
     std::ofstream tmpfile;
     tmpfile.open("tmppid.txt");
@@ -23,7 +27,32 @@ void Init() {
     std::cout << "Codeep has been successfully launched !!! \n"
               << "Enjoy coding !!!" << std::endl;
 
-    while (true) {}
+    char* kbd_file = getKeyboardDeviceFileName();
+    std::cout << "Keyboard File: " << kbd_file << std::endl;
+    int kbd_fd = openKeyboardDeviceFile(kbd_file);
+    assert(kbd_fd > 0);
+
+    if (daemon(1, 0) == -1) {
+        std::cout << "Something wrong ... :(" << std::endl;
+        exit(-1);
+    }
+    
+    input_event event; 
+    FILE *logfile = fopen(LOGFILE, "a");
+    setbuf(logfile, NULL);
+    int cnt = 0;
+    while (read(kbd_fd, &event, sizeof(input_event)) > 0) {
+        if (event.type == EV_KEY) {
+            cnt++;
+            std::string _cnt = std::to_string(cnt);
+            char* _c_cnt = new char[_cnt.length()+1];
+            strcpy(_c_cnt, _cnt.c_str());
+            fputs(_c_cnt, logfile);
+            delete [] _c_cnt;
+        }
+    }
+    close(kbd_fd);
+    fclose(logfile);
 }
 
 void Exit() {
--- src/core.hpp
+++ src/core.hpp
@@ -3,17 +3,19 @@
 
 #include <unistd.h>
 #include <errno.h>
+#include <linux/input.h>
 #include <iostream>
 
 namespace Codeep {
 namespace Core {
 
+//typedef struct input_event input_event;
 /*
  * Initialize a daemon process to record frequency of keystrokes
  */
 void Init();
 
-/*
+/* 
  * Kill the daemon process
  */
 void Exit();
