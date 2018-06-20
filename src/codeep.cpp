039e913d7bf79d551b76df3f2ef048f18580d955
huanghongru <huanghongru1997@gmail.com>
2018-06-20 22:57:01 +0800

    fix bug: cannot kill daemon process

--- src/codeep.cpp
+++ src/codeep.cpp
@@ -16,7 +16,7 @@ int help(int argc, char** argv) {
 
 int init(int argc, char **argv) {
     // 'Bonjour' means 'hello' in France.
-    rootCheck();
+    Codeep::Utils::rootCheck();
     cout << "Bonjour, Codeep!!" << endl;
     Codeep::Core::Init();
     return 0;
@@ -24,7 +24,7 @@ int init(int argc, char **argv) {
 
 int exit(int argc, char **argv) {
     // 'Au revoir' means 'goodbye' in France.
-    rootCheck();
+    // Codeep::Utils::rootCheck();
     cout << "Au revoir, Codeep!!" << endl;
     Codeep::Core::Exit();
     return 0;
